d9709f2fb220d449d8fa9bdd63de1bae5278c17b
Julie Fiess <juliefiess@gmail.com>
2025-01-10 09:26:26 -0600

    Some tests passing

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -8,7 +8,64 @@ public class Board {
         this.height = h;
     }
 
+    public Piece get(int x, int y) {
+        return this.board[x][y];
+    }
+
+    public boolean insertPiece(Piece newPiece, int insertX, int insertY) {
+        this.board[insertX][insertY] = newPiece;
+        return true;
+    }
+
+    public boolean checkBounds(int startX, int startY, int endX, int endY) {
+        if (startX < 0 || startX >= this.width || startY < 0 || startY >= this.height) {
+            return false;
+        }
+        if (endX < 0 || endX >= this.width || endY < 0 || endY >= this.height) {
+            return false;
+        }
+        return true;
+    }
+
     public boolean move(int startX, int startY, int deltaX, int deltaY) {
+        if (deltaX == 0 && deltaY == 0) {
+            return false;
+        }
+
+        if (startX < 0 || startX >= this.width || startY < 0 || startY >= 0) {
+            return false;
+        }
+
+        int endX = startX + deltaX;
+        int endY = startY + deltaY;
+
+        if (!checkBounds(startX, startY, endX, endY)) {
+            return false;
+        }
+
+        if (endX < 0 || endX >= this.width || endY < 0 || endY >= this.height) {
+            return false;
+        }
+
+        if (Math.abs(deltaX) + Math.abs(deltaX) > 1) {
+            return false;
+        }
+
+        if (this.board[endX][endY] instanceof Piece) {
+            if (deltaX == 1) {
+                endX += deltaX;
+            } else if (deltaY == 1) {
+                endY += deltaY;
+            }
+
+            if (!checkBounds(startX, startY, endX, endY)) {
+                return false;
+            }
+        }
+
+        this.board[endX][endY] = this.board[startX][startY];
+        this.board[startX][startY] = null;
+
         return true;
     }
 }
--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -1,17 +1,150 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
+    private Board game;
 
     @BeforeEach
     void setUp() {
+        game = new Board(8, 8);
     }
 
     @Test
-    void move() {
-        Board game = new Board(8, 8);
-        assertFalse(game.move(0,0,0,0));
+    void insertPieceA() {
+        Piece expected = Piece.A;
+
+        game.insertPiece(expected, 0, 0);
+
+        Piece actual = game.get(0, 0);
+        assertEquals(expected, actual);
+    }
+
+    @Nested
+    @DisplayName("Tests for move function")
+    class move {
+
+        @Test
+        void moveSame() {
+            assertFalse(game.move(0, 0, 0, 0));
+        }
+
+        @Test
+        void startOutOfBounds() {
+            assertFalse(game.move(9, 9, 8, 8));
+            assertFalse(game.move(-1, -1, 8, 8));
+        }
+
+        @Test
+        void endOutOfBounds() {
+            assertFalse(game.move(0, 0, 8, 8));
+            assertFalse(game.move(7, 7, -8, -8));
+        }
+
+        @Nested
+        @DisplayName("Tests for Moving Piece A")
+        class movePieceA {
+
+            private Piece expected = Piece.A;
+
+            @BeforeEach
+            void setupPieceA() {
+                game.insertPiece(expected, 0, 0);
+            }
+
+            @Nested
+            @DisplayName("All valid moves for Piece A")
+            class validMoves {
+
+                @Test
+                void validHorizontalMove() {
+                    game.move(0, 0, 1, 0);
+                    Piece actual = game.get(1, 0);
+
+                    assertEquals(expected, actual);
+                }
+
+                @Test
+                void validVerticalMove() {
+                    game.move(0, 0, 0, 1);
+                    Piece actual = game.get(0, 1);
+
+                    assertEquals(expected, actual);
+                }
+
+            }
+
+            @Test
+            void invalidMoves() {
+                assertFalse(game.move(0,0,1,1));
+            }
+
+            @Test
+            void testCollision() {
+                Piece collider = Piece.A;
+                game.insertPiece(collider, 1, 0);
+
+                game.move(0,0,1,0); // Collides on the right
+
+                Piece actual = game.get(2,0);
+                assertEquals(expected, actual);
+            }
+
+            @Test
+            void testCollisionOffBoard() {
+                game.insertPiece(expected, 7, 6);
+
+                Piece collider = Piece.A;
+                game.insertPiece(collider, 7,7);
+
+                game.move(7,6,0,1);
+                Piece actual = game.get(7,6);
+                assertEquals(expected, actual);
+            }
+
+/*
+            @Nested
+            @DisplayName("All valid moves for Piece B")
+            class validMoves {
+
+                @Test
+                void validHorizontalMove() {
+                    game.move(0, 0, 1, 0);
+                    Piece actual = game.get(1, 0);
+
+                    assertEquals(expected, actual);
+                }
+
+                @Test
+                void validVerticalMove() {
+                    game.move(0, 0, 0, 1);
+                    Piece actual = game.get(0, 1);
+
+                    assertEquals(expected, actual);
+                }
+
+            }
+
+            @Test
+            void invalidMoves() {
+                assertFalse(game.move(0,0,1,1));
+            }
+
+            @Test
+            void testCollision() {
+                Piece collider = Piece.A;
+                game.insertPiece(collider, 1, 0);
+
+                game.move(0,0,1,0); // Collides on the right
+
+                Piece actual = game.get(2,0);
+                assertEquals(expected, actual);
+            }
+
+ */
+        }
     }
 }
